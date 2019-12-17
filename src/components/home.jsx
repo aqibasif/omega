09404f594332ed00ea998570aaed300f26638b49
Aqib Asif <mirza_aqib@live.com>
2019-12-17 15:25:58 +0500

    Heade

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,8 +4,8 @@ class Home extends Component {
     state = {  }
     render() { 
         return ( 
-            <div className='back'>
-                <img src="/img/omega11-2.jpg" alt="OMEGA" width="100%"/>
+            <div className='home'>
+                <img src="/img/omega114.jpg" alt="OMEGA" width="100%"/>
 
             </div>
          );
