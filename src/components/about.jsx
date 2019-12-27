2e4428320d45eb3d06cbe7ba6dc6250a92e54bcd
Aqib Asif <mirza_aqib@live.com>
2019-12-27 19:35:01 +0500

    Updated

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -14,6 +14,9 @@ class About extends Component {
             <div className='col-sm-6'>
                <img src='/img/omega114.jpg' alt='cocomo' width='100%' />
 
+               <img src='/img/img3.jpg' alt='cocomo' width='100%' />
+
+
                <p className='m-5 abouttext'>
                   The key point to take away here is the variant prop on our
                   motion element. Variants let us define multiple states for our
@@ -21,28 +24,6 @@ class About extends Component {
                   component can be in. By giving a component and its children
                   variants with matching names, whole React trees can be
                   animated by changing a single prop. By using variants, a
-                  parent can easily orchestrate the animations of its children
-                  with special transition props like staggerChildren. Variants
-                  can also be dynamic functions that return different props
-                  based on data passed to each component’s custom prop. Note:
-                  Notice how we’re using hooks. If hooks are new to you, check
-                  out this. The key point to take away here is the variant prop
-                  on our motion element. Variants let us define multiple states
-                  for our animations. Variants are pre-defined visual states
-                  that a component can be in. By giving a component and its
-                  children variants with matching names, whole React trees can
-                  be animated by changing a single prop. By using variants, a
-                  parent can easily orchestrate the animations of its children
-                  with special transition props like staggerChildren. Variants
-                  can also be dynamic functions that return different props
-                  based on data passed to each component’s custom prop. Note:
-                  Notice how we’re using hooks. If hooks are new to you, check
-                  out this. The key point to take away here is the variant prop
-                  on our motion element. Variants let us define multiple states
-                  for our animations. Variants are pre-defined visual states
-                  that a component can be in. By giving a component and its
-                  children variants with matching names, whole React trees can
-                  be animated by changing a single prop. By using variants, a
                   parent can easily orchestrate the animations of its children
                   with special transition props like staggerChildren. Variants
                   can also be dynamic functions that return different props
