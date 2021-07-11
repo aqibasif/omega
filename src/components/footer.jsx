516a421cba31370da6167abb0b070fd89d96bb04
Aqib Asif <aqib.asif@approcket.co>
2021-07-11 12:54:42 +0500

    new

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
             </Link>
          </div>
          <div className='copyright'>
-            <p>Copyright © 2020 Omega - Developed by <a href="https://aqibasif.vercel.app" target="_blank">Muhammad Aqib Asif</a></p>
+            <p>Copyright © 2020 Omega - Developed by <a href="https://aqibasif.vercel.app" rel="noopener noreferrer" target="_blank">Muhammad Aqib Asif</a></p>
          </div>
       </React.Fragment>
    );
